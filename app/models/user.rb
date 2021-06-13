44d8c3e2de6288eec17aa808e214c8c06a2ab717
vagrant <vagrant@x.dev>
2021-06-13 06:12:52 +0000

    user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,26 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :username, uniqueness: true
   validates :first_name, presence: true
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :email, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: "must be a valid email address."
+  }
+  validates :must_not_be_underage
+  validates :hashed_password
+
+  def must_not_be_underage
+    age = birthday.year - Time.current.year
+    return if age < 18
+
+    errors.add(:base, "must be at least 17 years old")
+  end
+
+  def password_never_be_used_before
+    reutrn if PastPassword.where(
+      user: self,
+      hashed_password: Digest::MD5.hexdigest(password)
+    ).blank?
+
+    errors.add(:password, "must be a new password")
+  end
 end
